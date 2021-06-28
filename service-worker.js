3c8e969777a137ef44e67b5843f4b8a6a08a8ade
Daniel Russ <druss@mail.nih.gov>
2021-06-28 14:04:55 -0400

    added the sw back + fix sw

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,6 @@
 console.log('Hello from service-worker.js');
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.1.5/workbox-sw.js');
 
-// This will trigger the importScripts() for workbox.strategies and its dependencies:
-workbox.loadModule('workbox-strategies');
+const { registerRoute } = workbox.routing;
+
+registerRoute(/.+/, new NetworkFirst({ cacheName: 'tmpCache' }))
