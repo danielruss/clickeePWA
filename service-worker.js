29edf856af1344e537d85c3a30369e8bad1d945c
Daniel Russ <druss@mail.nih.gov>
2021-06-30 11:22:26 -0400

    update manifest

--- service-worker.js
+++ service-worker.js
@@ -2,5 +2,23 @@ console.log('Hello from service-worker.js');
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.1.5/workbox-sw.js');
 
 const { registerRoute } = workbox.routing;
+const { CacheFirst, NetworkFirst, StaleWhileRevalidate, NetworkOnly } = workbox.strategies;
+const { CacheableResponse, CacheableResponsePlugin } = workbox.cacheableResponse;
+const { ExpirationPlugin } = workbox.expiration;
+const { BackgroundSyncPlugin } = workbox.backgroundSync
 
-//registerRoute(/.+/, new NetworkFirst({ cacheName: 'tmpCache' }))
+
+registerRoute(/\.(?:js|css)$/, new NetworkFirst({ cacheName: 'static-cache' }));
+registerRoute(/\.(?:png|jpg|jpeg|svg|gif|ico)$/,
+    new CacheFirst({
+        cacheName: 'images-cache',
+        plugins: [
+            new ExpirationPlugin({
+                maxEntries: 30,
+                maxAgeSeconds: 7 * 24 * 60 * 60,
+            })
+        ]
+    })
+);
+
+workbox.precaching.precacheAndRoute([{ url: 'index.html', revision: 'v1.0' }]);
